8710a7bac002f16afe6ac5070067b0b0f349bc4e
Gina Häußge <gina@octoprint.org>
2019-06-05 16:53:22 +0200

    We also need a serve parameter

--- assets/js/carbonloader.js
+++ assets/js/carbonloader.js
@@ -1,9 +1,10 @@
 (function() {
+    var serve = new URLSearchParams(location.search).get("serve");
     var placement = new URLSearchParams(location.search).get("placement");
     var po = document.createElement("script");
     po.type = "text/javascript";
     po.async = true;
-    po.src = "//cdn.carbonads.com/carbon.js?serve=CK7DT2QE&placement=" + placement;
+    po.src = "//cdn.carbonads.com/carbon.js?serve=" + serve + "&placement=" + placement;
     po.id = "_carbonads_js";
     var s = document.currentScript;
     s.parentNode.insertBefore(po, s);
