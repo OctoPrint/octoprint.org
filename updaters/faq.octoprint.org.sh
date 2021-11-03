d31620e86e617746340073bc6253ac8462d530df
Gina Häußge <gina@foosel.net>
2021-11-03 18:57:36 +0100

    ✨ Add management for faq.octoprint.org

---
+++ updaters/faq.octoprint.org.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+CONFIG="$1"
+SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+
+source "$SCRIPT_DIR/../helpers.sh"
+
+DATA_FILE="$SCRIPT_DIR/../data/faq.octoprint.org/mapping.json"
+
+ENTRIES=$(cat $DATA_FILE | jq -r "to_entries | map(\"\tlocation /\" + .key + \" {\n\t\treturn 301 \" + .value + \";\n\t}\n\") | join(\"\n\")")
+
+echo "Entries:"
+echo "$ENTRIES"
+
+prefix=$(sed '/# managed by updater/q' "$CONFIG")
+suffix=$(sed '/# \/ managed by updater/,$!d' "$CONFIG")
+
+echo "$prefix" > "$CONFIG"
+echo "$ENTRIES" >> "$CONFIG"
+echo "$suffix" >> "$CONFIG"
