8c70d019b8e82991634526e83639d69511655703
Gina Häußge <gina@foosel.net>
2021-11-08 10:57:38 +0100

    ⚡️ Only reload nginx if there were changes

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,20 @@ fi
 
 NGINX_SITES=/etc/nginx/sites-available
 
+modified=0
 for site in $(ls ./nginx); do
     echo "Updating $site..."
+    hash_before=$(md5sum $NGINX_SITES/$site)
     cp ./nginx/$site $NGINX_SITES/$site
     [ -f ./updaters/$site.sh ] && echo "Found updater, running it..." && ./updaters/$site.sh $NGINX_SITES/$site
+    hash_after=$(md5sum $NGINX_SITES/$site)
+    if [ "$hash_before" != "$hash_after" ]; then
+        echo "Site $site has been modified, reloading nginx..."
+        modified=1
+    fi
 done
 
-systemctl reload nginx
+if [ $modified -eq 1 ]; then
+    echo "Reloading nginx..."
+    systemctl reload nginx
+fi
--- update.sh
+++ update.sh
@@ -9,9 +9,23 @@ fi
 
 NGINX_SITES=/etc/nginx/sites-available
 
+modified=0
 for updater in $(ls ./updaters); do
     site=$(basename $updater .sh)
-    [ -f "$NGINX_SITES/$site" ] && echo "Running updater $updater..." && ./updaters/$updater $NGINX_SITES/$site
+    config="$NGINX_SITES/$site"
+    if [ -f "$config" ]; then
+        echo "Running updater $updater..."
+        hash_before=$(md5sum "$config")
+        ./updaters/$updater "$config"
+        hash_after=$(md5sum "$config")
+        if [ "$hash_before" != "$hash_after" ]; then
+            echo "Updater $updater modified $config"
+            modified=1
+        fi
+    fi
 done
 
-systemctl reload nginx
+if [ $modified -eq 1 ]; then
+    echo "Reloading nginx..."
+    systemctl reload nginx
+fi
