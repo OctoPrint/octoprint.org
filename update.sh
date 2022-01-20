0620979dc5d44cce89ab2a21c91f889332d22f7d
Gina Häußge <gina@foosel.net>
2022-01-20 13:09:38 +0100

    Use script path instead of relative calls

--- update.sh
+++ update.sh
@@ -7,16 +7,18 @@ if [ "$EUID" -ne 0 ]; then
     exit
 fi
 
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
 NGINX_SITES=/etc/nginx/sites-available
 
 modified=0
-for updater in $(ls ./updaters); do
+for updater in $(ls $SCRIPTPATH/updaters); do
     site=$(basename $updater .sh)
     config="$NGINX_SITES/$site"
     if [ -f "$config" ]; then
         echo "Running updater $updater..."
         hash_before=$(md5sum "$config")
-        ./updaters/$updater "$config"
+        $SCRIPTPATH/updaters/$updater "$config"
         hash_after=$(md5sum "$config")
         if [ "$hash_before" != "$hash_after" ]; then
             echo "Updater $updater modified $config"
