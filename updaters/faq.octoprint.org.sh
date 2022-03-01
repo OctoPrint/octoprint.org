5106c2f94c8d8ccc2a00cec01b581b52fc16611c
Gina Häußge <gina@foosel.net>
2022-03-01 19:01:21 +0100

    Set no cache and 302 on FAQ redirects

--- updaters/faq.octoprint.org.sh
+++ updaters/faq.octoprint.org.sh
@@ -9,7 +9,7 @@ source "$SCRIPT_DIR/../helpers.sh"
 
 DATA_FILE="$SCRIPT_DIR/../data/faq.octoprint.org/mapping.json"
 
-ENTRIES=$(cat $DATA_FILE | jq -r "to_entries | map(\"\tlocation /\" + .key + \" {\n\t\treturn 301 \" + .value + \";\n\t}\n\") | join(\"\n\")")
+ENTRIES=$(cat $DATA_FILE | jq -r "to_entries | map(\"\tlocation /\" + .key + \" {\n\t\texpires 0d;\n\t\tadd_header Cache-Control \\\"no-cache\\\";\n\t\treturn 302 \" + .value + \";\n\t}\n\") | join(\"\n\")")
 
 prefix=$(sed '/# managed by updater/q' "$CONFIG")
 suffix=$(sed '/# \/ managed by updater/,$!d' "$CONFIG")
