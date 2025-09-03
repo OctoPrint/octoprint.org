8c1ec2c5052aa862f57ee9f6bfb89c40469f83ed
Gina Häußge <gina@octoprint.org>
2025-09-03 12:11:45 +0200

    chore: update get.octoprint.org for new branch strat
    Also added missing /python-eol redirect

--- updaters/get.octoprint.org.sh
+++ updaters/get.octoprint.org.sh
@@ -17,39 +17,30 @@ stable_major_minor=$(echo $stable | awk -F "." '{print $1 "." $2}')
 last=$(echo $DATA | jq -r "[ .[] | select(.prerelease != true and (.tag_name | startswith(\"$stable_major_minor.\") | not)) ][0].tag_name")
 
 if [ $stable_index -ne 0 ]; then
-    maintenance=$(echo $DATA | jq -r ".[0:$stable_index] | [ .[] | select(.prerelease and .target_commitish == \"rc/maintenance\") ][0].tag_name")
-    devel=$(echo $DATA | jq -r ".[0:$stable_index] | [ .[] | select(.prerelease and (.target_commitish == \"rc/maintenance\" or .target_commitish == \"rc/devel\")) ][0].tag_name")
+    next=$(echo $DATA | jq -r ".[0:$stable_index] | [ .[] | select(.prerelease and (.target_commitish == \"next\" or .target_commitish == \"rc/maintenance\" or .target_commitish == \"rc/devel\")) ][0].tag_name")
 else
-    maintenance=$stable
-    devel=$stable
+    next=$stable
 fi
 
 echo "Stable: $stable (index: $stable_index, major.minor: $stable_major_minor)"
-echo "Maintenance: $maintenance"
-echo "Devel: $devel"
+echo "Next: $next"
 echo "Last: $last"
 
 current_stable=$(get_nginx_var "stable" "$CONFIG")
-current_maintenance=$(get_nginx_var "maintenance" "$CONFIG")
-current_devel=$(get_nginx_var "devel" "$CONFIG")
+current_next=$(get_nginx_var "next" "$CONFIG")
 current_last=$(get_nginx_var "last" "$CONFIG")
 
 echo "Current stable: $current_stable"
-echo "Current maintenance: $current_maintenance"
-echo "Current devel: $current_devel"
+echo "Current next: $current_maintenance"
 echo "Current last: $current_last"
 
 if [[ "$stable" != "$current_stable" ]]; then
     echo "Setting stable to $stable..."
     set_nginx_var "stable" "$stable" $CONFIG
 fi
-if [[ "$maintenance" != "$current_maintenance" ]]; then
-    echo "Setting maintenance to $maintenance..."
-    set_nginx_var "maintenance" "$maintenance" $CONFIG
-fi
-if [[ "$devel" != "$current_devel" ]]; then
-    echo "Setting devel to $devel..."
-    set_nginx_var "devel" "$devel" $CONFIG
+if [[ "$next" != "$current_next" ]]; then
+    echo "Setting next to $next..."
+    set_nginx_var "next" "$next" $CONFIG
 fi
 if [[ "$last" != "$current_last" ]]; then
     echo "Setting last to $last..."
